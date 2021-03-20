a450e294fa476ff32c4d197d213a9b2900ef8750
Marble <mrivera0014@gmail.com>
2021-03-19 18:11:30 -0600

    database is created

---
+++ public/db.js
@@ -0,0 +1,57 @@
+let db;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = event => {
+    const db = event.target.result;
+
+    db.createObjectStore('pending', { autoIncrement: true })
+}
+
+request.onsuccess = (event) => {
+    // console.log("resultbase successful?")
+    db = event.target.result
+
+    if (navigator.onLine) {
+        checkresultbase()
+    }
+}
+
+request.onerror = (event) => {
+    console.log(event.target.errorCode)
+}
+
+const saveRecord = (record) => {
+    const transaction = db.transaction(['pending'], 'readwrite')
+
+    const store = transaction.createObjectStore('pending')
+
+    store.add(record)
+}
+
+const checkresultbase = () => {
+    const transaction = db.transaction(['pending'], 'readwrite')
+
+    const store = transaction.objectStore('pending')
+
+    const getAll = store.getAll()
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(['pending'], 'readwrite')
+                })
+        }
+    }
+}
+
+addEventListener('online', checkresultbase)
